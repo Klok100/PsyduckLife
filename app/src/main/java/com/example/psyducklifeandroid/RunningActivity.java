220a7a44921e68e2c96c2dda120beea1c704218a
klok101 <blackhawks4447@gmail.com>
2019-10-08 11:59:44 -0500

    Start of the Infinite Background Scroller

--- app/src/main/java/com/example/psyducklifeandroid/RunningActivity.java
+++ app/src/main/java/com/example/psyducklifeandroid/RunningActivity.java
@@ -2,7 +2,10 @@ package com.example.psyducklifeandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.ValueAnimator;
 import android.os.Bundle;
+import android.view.animation.LinearInterpolator;
+import android.widget.ImageView;
 
 public class RunningActivity extends AppCompatActivity {
 
@@ -10,5 +13,25 @@ public class RunningActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_running);
+
+        final ImageView backgroundOne = findViewById(R.id.background_one);
+        final ImageView backgroundTwo = findViewById(R.id.background_two);
+
+        final ValueAnimator animator = ValueAnimator.ofFloat(0.0f, 1.0f);
+        animator.setRepeatCount(ValueAnimator.INFINITE);
+        animator.setInterpolator(new LinearInterpolator());
+        animator.setDuration(100000L);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                final float progress = (float) animation.getAnimatedValue();
+                final float width = backgroundOne.getWidth();
+                final float translationX = width * progress;
+                backgroundOne.setTranslationX(translationX);
+                backgroundTwo.setTranslationX(translationX - width);
+            }
+        });
+        animator.start();
+
     }
 }
