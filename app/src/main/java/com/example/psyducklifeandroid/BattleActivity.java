5507ba3bc85650da99a9fcbd42a7ad0bb30c57d1
klok100@icloud.com <Klok@101>
2019-10-22 12:29:13 -0500

    Push battle stuff 10/22/19

--- app/src/main/java/com/example/psyducklifeandroid/BattleActivity.java
+++ app/src/main/java/com/example/psyducklifeandroid/BattleActivity.java
@@ -4,7 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class BattleActivity extends AppCompatActivity {
+
+    private static ArrayList<EnemyPokemon> enemyPokemon = new ArrayList<>();
+    private static ArrayList<Moves> onixMoves = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +22,18 @@ public class BattleActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
         Psyduck psyduck = new Psyduck();
-        int randEnemy = (int) Math.floor(Math.random() * 4) + 1;
 
+        enemyPokemon.add(new EnemyPokemon("Onix", 35, 45, 160, 30, 45, 70));
+        enemyPokemon.add(new EnemyPokemon("Starmie", 60, 75, 85, 100, 85, 115 ));
+        enemyPokemon.add(new EnemyPokemon("Raichu", 60, 90, 55, 90, 80, 100));
+        enemyPokemon.add(new EnemyPokemon("Vileplume", 75, 80, 85, 100, 90, 55));
+        enemyPokemon.add(new EnemyPokemon("Venomoth", 70, 65, 60, 90, 75, 90));
+        enemyPokemon.add(new EnemyPokemon("Alakazam", 55, 50, 45, 135, 85, 120));
+        enemyPokemon.add(new EnemyPokemon("Arcanine", 90, 110, 80, 100, 80, 95));
+        enemyPokemon.add(new EnemyPokemon("Nidoqueen", 90, 82, 87, 75, 85, 76));
+
+        onixMoves.add(new Moves("Stone Edge", "rock", 100, 80));
+        
 
     }
 }
--- app/src/main/java/com/example/psyducklifeandroid/EnemyPokemon.java
+++ app/src/main/java/com/example/psyducklifeandroid/EnemyPokemon.java
@@ -6,27 +6,24 @@ import java.util.ArrayList;
 public class EnemyPokemon {
 
     public String name;
+    public String type;
     public int hp;
     public int atk;
     public int def;
     public int spatk;
     public int spdef;
     public int spd;
-    public ArrayList<EnemyPokemon> enemyPokemon = new ArrayList<>();
 
     public EnemyPokemon(String name, int hp, int atk, int def, int spatk, int spdef, int spd){
         this.name = name;
+        this.type = type;
         this.hp = hp;
         this.atk = atk;
         this.def = def;
         this.spatk = spatk;
         this.spdef = spdef;
         this.spd = spd;
-        enemyPokemon.add(new EnemyPokemon("Onix", 35, 45, 160, 30, 45, 70));
-        enemyPokemon.add(new EnemyPokemon("Starmie", 60, 75, 85, 100, 85, 115 ));
-        enemyPokemon.add(new EnemyPokemon("Raichu", 60, 90, 55, 90, 80, 100));
-        enemyPokemon.add(new EnemyPokemon("Vileplume", 75, 80, 85, 100, 90, 55));
-        
+
     }
 
     public String getName() {
