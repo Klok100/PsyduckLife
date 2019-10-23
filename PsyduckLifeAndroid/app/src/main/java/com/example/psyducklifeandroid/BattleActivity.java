012d40c9c7f279b8240be8948e8004c776a97c5d
Nathan Home <klok100@icloud.com>
2019-10-22 22:09:16 -0500

    Commit Test Home 10/22/19

--- PsyduckLifeAndroid/app/src/main/java/com/example/psyducklifeandroid/BattleActivity.java
+++ PsyduckLifeAndroid/app/src/main/java/com/example/psyducklifeandroid/BattleActivity.java
@@ -3,6 +3,7 @@ package com.example.psyducklifeandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class BattleActivity extends AppCompatActivity {
 
@@ -10,5 +11,28 @@ public class BattleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_battle);
+    }
+
+    public int attack(View v){
+
+    }
+
+    public void scratch(){
+        double enemyAtk = Math.floor(Math.random() * 4) + 1;
+        if (enemyAtk == 1){
+
+        }
+
+        else if (enemyAtk == 2){
+
+        }
+
+        else if (enemyAtk == 3){
+
+        }
+
+        else if (enemyAtk == 4){
+
+        }
     }
 }
